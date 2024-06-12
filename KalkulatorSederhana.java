d98fcb1524db23ed1e5be4380f7a389a809a1bd8
Jayfline Hutagalung <jayfline7@gmail.com>
2024-06-12 14:50:10 +0700

    Memperbaiki bug pada program

--- KalkulatorSederhana.java
+++ KalkulatorSederhana.java
@@ -4,63 +4,47 @@ public class KalkulatorSederhana {
     public static void main(String[] args) {
         Scanner inputanUser = new Scanner(System.in);
 
-        double pilihan;
+        int pilihan = 1;
 
-         do {
-             System.out.println("Apakah anda masih ingin menggunakan aplikasi ini?");
-             System.out.println("1. Ya\n2. Tidak");
-             pilihan = inputanUser.nextDouble();
+                System.out.println("Selamat datang di Kalkulator Sederhana saya!!!");
+        do {
 
-             if (pilihan==1){
-                 System.out.print("Masukkan angka pertama: ");
-                 double angkaPertama = inputanUser.nextDouble();
+            if (pilihan==1){
+                System.out.print("Masukkan angka pertama: ");
+                double angkaPertama = inputanUser.nextDouble();
 
-                 System.out.print("Masukkan angka kedua: ");
-                 double angkaKedua = inputanUser.nextDouble();
+                System.out.print("Masukkan angka kedua: ");
+                double angkaKedua = inputanUser.nextDouble();
 
-                 System.out.println("Pilih Operator berikut: ");
-                 System.out.println("1. Penjumlahan\n2. Pengurangan\n3. Perkalian \n4. Pembagian");
-                 System.out.print("Masukkan kode operator:");
-                 int pilihanOperator = inputanUser.nextInt();
+                System.out.println("Pilih Operator berikut: ");
+                System.out.println("1. Penjumlahan\n2. Pengurangan\n3. Perkalian \n4. Pembagian");
+                System.out.print("Masukkan kode operator:");
+                int pilihanOperator = inputanUser.nextInt();
 
-                 switch (pilihanOperator) {
-                     case 1 -> System.out.println("Hasil: " + (angkaPertama + angkaKedua));
-                     case 2 -> System.out.println("Hasil: " + (angkaPertama - angkaKedua));
-                     case 3 -> System.out.println("Hasil: " + angkaPertama * angkaKedua);
-                     case 4 -> {
-                         if(angkaKedua!=0){
-                             double hasilBagi = angkaPertama/angkaKedua;
-                             System.out.println(hasilBagi);
-                         } else {
-                             System.out.println("Pembagian dengan angka 0 tidak diperbolehkan");
-                         }
-                     }
-                     default -> System.out.println("Kode operator tidak dikenal. Masukkan angka antara 1 sampai 4");
-                 }
-                 try {
-                     Thread.sleep(1500); // Program akan menunggu selama 1500 milisecond
-                 } catch (InterruptedException e) {
-                     System.out.println("Terjadi kesalahan saat menunggu.");
-                 }
-             } else if (pilihan == 2) {
-                 System.out.println("Terima kasih telah menggunakan kalkulator ini");
-                 break;
-             } else {
-                 System.out.println("Inputan anda tidak dikenali program (Diluar pilihan yang telah diberikan)!");
-                 break;
-             }
+                switch (pilihanOperator) {
+                    case 1 -> System.out.println("Hasil: " + (angkaPertama + angkaKedua));
+                    case 2 -> System.out.println("Hasil: " + (angkaPertama - angkaKedua));
+                    case 3 -> System.out.println("Hasil: " + angkaPertama * angkaKedua);
+                    case 4 -> {
+                        if(angkaKedua!=0){
+                            double hasilBagi = angkaPertama/angkaKedua;
+                            System.out.println("Hasil: " + hasilBagi);
+                        } else {
+                            System.out.println("Pembagian dengan angka 0 tidak diperbolehkan");
+                        }
+                    }
+                    default -> System.out.println("Kode operator tidak dikenal. Masukkan angka antara 1 sampai 4");
+                }
+                System.out.println("Apakah anda masih ingin menggunakan aplikasi ini?");
+                System.out.println("1. Ya\n2. Tidak");
+                pilihan = inputanUser.nextInt();
 
-             System.out.println("Apakah anda masih ingin menggunakan aplikasi ini?");
-             System.out.println("1. Ya\n2. Tidak");
-             pilihan = inputanUser.nextInt();
+                if (pilihan ==2 ){
+                    System.out.println("Terima kasih telah menggunakan kalkulator ini.");
+                    inputanUser.close();
+                }
+            }
 
         }while(pilihan == 1);
-
-         if (pilihan ==2 ){
-             System.out.println("Terima kasih telah menggunakan kalkulator ini.");
-         }
-        }
+    }
 }
-
-
-
